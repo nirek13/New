c27de5ebe0b07990408c498e3a8d4e8430bd4073
nirek13 <nirekshetty@gmail.com>
2024-08-03 20:29:01 -0400

    D

--- assets/JS/photogallery.js
+++ assets/JS/photogallery.js
@@ -1,29 +1,14 @@
-// Function to fetch images and create gallery
-function createPhotoGallery() {
-    const galleryContainer = document.getElementById('gallery-images');
+// Include Lightbox
+import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
 
-    // Fetch the list of files in the PhotoGallery directory
-    fetch('/Users/nirekshetty/Downloads/NirekshettySite/assets/PhotoGallery')
-        .then(response => response.text()) // Get the directory listing as text
-        .then(text => {
-            // Parse the directory listing text to extract filenames
-            const filenames = text.split('\n').filter(name => name.trim() !== ''); // Remove empty lines
+const lightbox = new PhotoSwipeLightbox({
+    // may select multiple "galleries"
+    gallery: '#gallery--getting-started',
 
-            // Loop through the filenames and create HTML elements for each image
-            filenames.forEach(filename => {
-                const imageElement = document.createElement('div');
-                imageElement.classList.add('col-md-4'); // Adjust column size as needed
+    // Elements within gallery (slides)
+    children: 'a',
 
-                const img = document.createElement('img');
-                img.src = `/Users/nirekshetty/Downloads/NirekshettySite/assets/PhotoGallery/${filename}`;
-                img.alt = filename; // Set alt text to image filename
-
-                imageElement.appendChild(img);
-                galleryContainer.appendChild(imageElement);
-            });
-        })
-        .catch(error => console.error('Error fetching images:', error));
-}
-
-// Call the function to create the photo gallery when the page loads
-window.addEventListener('DOMContentLoaded', createPhotoGallery);
+    // setup PhotoSwipe Core dynamic import
+    pswpModule: () => import('/photoswipe/photoswipe.esm.js')
+});
+lightbox.init();
