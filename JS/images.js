03eadc86bca6f201c7ba7b0c7fddb303176cb84d
nirek13 <nirekshetty@gmail.com>
2024-12-26 17:04:28 -0500

    s

--- JS/images.js
+++
@@ -1,5 +0,0 @@
-// Disable image dragging and right click context menu
-$('img').on('dragstart', function (event) { event.preventDefault(); });
-$('img').bind('contextmenu', function (e) {
-    return false;
-});
--- JS/loader.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    // Example: Hide the loading spinner after 3 seconds
-    setTimeout(function() {
-        const loadingContainer = document.querySelector('.loading-container');
-        loadingContainer.style.display = 'none';
-    }, 3000);
-});
