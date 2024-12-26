116e07b9f7be55b4596bd8877912e6fd2a68bfe2
Nirek Shetty <63679448+nirek13@users.noreply.github.com>
2024-12-26 17:07:58 -0500

    Delete Backend.ts

--- Backend.ts
+++
@@ -1,5 +0,0 @@
-fetch('https://canadabuys.canada.ca/opendata/pub/newTenderNotice-nouvelAvisAppelOffres.csv')
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);  // This will log the CSV content from the URL
-    });
